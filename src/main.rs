8e2f83e41bc4783bb7f0fa46520fefdf2c28c46b
Sindre Ilebekk Johansen <sindre@iterate.no>
2021-08-20 12:34:52 +0200

    Simplify code

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,17 @@ async fn main() {
 async fn start() {
     let app = Router::new()
         .route("/a", get(get_a))
-        .route("/b", get(get_b))
-        .route("/c", get(get_c))
-        .route("/d", get(get_d))
-        .route("/e", get(get_e))
-        .route("/f", get(get_f))
-        .route("/g", get(get_g))
-        .route("/h", get(get_h))
-        .route("/h", get(get_h))
-        .route("/h", get(get_h))
-        .route("/h", get(get_h))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
+        .route("/a", get(get_a))
         // Remove boxed, and the `cargo check` goes from multiple minutes to around a second.
         .boxed();
 
@@ -28,43 +29,6 @@ async fn start() {
         .unwrap();
 }
 
-// GET /api/v1/apps
 async fn get_a() -> &'static str {
-    "ok"
-}
-
-// GET /api/v1/apps/{git_owner}/{git_name}
-async fn get_b() -> &'static str {
-    "ok"
-}
-
-// GET /api/v1/deployment/{id}
-async fn get_c() -> &'static str {
-    "ok"
-}
-// GET /api/v1/builds
-async fn get_aa() -> &'static str {
-    "ok"
-}
-
-// GET /api/v1/builds/{build_id}
-async fn get_e() -> &'static str {
-    "ok"
-}
-
-// GET /api/v1/secrets/publickey
-async fn get_h() -> &'static str {
-    "ok"
-}
-
-async fn get_f() -> &'static str {
-    "ok"
-}
-
-async fn get_g() -> &'static str {
-    "ok"
-}
-
-async fn get_d() -> &'static str {
     "ok"
 }
